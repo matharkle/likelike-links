c56b67e788a8cbac52a0e1bdc8427b5c932635a7
matharkle <christianmh@vcu.edu>
2024-12-14 03:08:08 -0500

    structure FINAL final

--- structure-FINAL/scripts/main.js
+++ structure-FINAL/scripts/main.js
@@ -1,7 +1,7 @@
 $(".draggable").draggable({});
 
-function playFunction() {
-    const audio = new Audio("audio/fabulous.m4a");
+function staticSound() {
+    const audio = new Audio("audio/static.m4a");
     audio.play(setTimeout);
 }
 
@@ -11,5 +11,57 @@ function zuneOn () {
 
 function keyboardSound () {
     const audio = new Audio("audio/keyboard.m4a");
+    audio.play(setTimeout);
+}
+
+// function ispySpin () {
+//     $(".ispy").toggleClass("spin");
+// }
+
+function pjSam () {
+    $(".pjsam").toggleClass("spin");
+    const audio = new Audio("audio/pj-sam.m4a");
+    audio.play(setTimeout);
+}
+
+function carsSong () {
+    const audio = new Audio("audio/cars.m4a");
+    audio.play(setTimeout);
+}
+
+function mouseClick () {
+    const audio = new Audio("audio/mouse.m4a");
+    audio.play(setTimeout);
+}
+
+function alligatorSound () {
+    const audio = new Audio("audio/alligator.m4a");
+    audio.play(setTimeout);
+}
+
+function wheelTurn () {
+    $(".wheel").toggleClass("turn");
+    const audio = new Audio("audio/item-block.m4a");
+    audio.play(setTimeout);
+}
+
+function wiimote () {
+    $(".wiimote").toggleClass("shake");
+    const audio = new Audio("audio/mario.m4a");
+    audio.play(setTimeout);
+}
+
+function cityfolkSong () {
+    const audio = new Audio("audio/city-folk.m4a");
+    audio.play(setTimeout);
+}
+
+function badLlama () {
+    const audio = new Audio("audio/bad-llama.m4a");
+    audio.play(setTimeout);
+}
+
+function walkieTalkie () {
+    const audio = new Audio("audio/walkie-talkie.m4a");
     audio.play(setTimeout);
 }
